b8fe2e892af77c774e3ebf16242b8b031d90695d
lubin_roineau <lubin.roineau@laposte.net>
2022-06-30 17:21:17 +0200

    fixe several bugs but still issue on display rotating ellipse

--- gnssr4river/Fresnel/FresnelZone.py
+++ gnssr4river/Fresnel/FresnelZone.py
@@ -13,11 +13,11 @@ from matplotlib.patches import Ellipse
 import math as m
 
 # Usefull constants
-c = 299792458  # m.s-1 Speed of light
-L1_freq = 1575.42e6  # Hz L1 frequency
-L2_freq = 1227.60e6  # Hz L2 frequency
-lambda_L1 = (c/L1_freq)  # m wavelenght for L1
-lambda_L2 = (c/L2_freq)  # m wavelenght for L2
+c = 299792458               # m.s-1 Speed of light
+L1_freq = 1575.42e6         # Hz L1 frequency
+L2_freq = 1227.60e6         # Hz L2 frequency
+lambda_L1 = (c/L1_freq)     # m wavelenght for L1
+lambda_L2 = (c/L2_freq)     # m wavelenght for L2
 
 
 # Calculation of the First Fresnel Zone
@@ -64,13 +64,13 @@ def FirstFresnelZone(freq, h, elev):
     # semi-majpr axis
     a = b/np.sin(m.radians(elev))
 
-    # determine distance to ellipse center
-    R = (h + d/(np.sin(elev) * np.pi/180)) / (np.tan(elev) * np.pi/180)
+    # determine distance to ellipse center in meters
+    R = (h + d/np.sin(m.radians(elev))) / np.tan(m.radians(elev))
 
     return a, b, R
 
 
-def plotEllipse(a, b, R, azim):
+def plotEllipse(a, b, R, azim, color):
     """
     Plots an ellipse
     
@@ -82,23 +82,88 @@ def plotEllipse(a, b, R, azim):
     We assume the receiver is located at coordinates (0,0), y axis for North and x axis for East
     """
 
+    if azim > 360 or azim < 0:
+        raise Exception("Wrong value azim, should be between 0 and 360!")  
+
     azim = azim *np.pi/180
-    xR = R*np.sin(azim)  # x-position of the center
-    yR = R*np.cos(azim)  # y-position of the center
-    theta = (90*np.pi/180)+azim  # rotation angle
+    #xR = R*np.sin(azim)  # x-position of the center
+    #yR = R*np.cos(azim)  # y-position of the center
+    theta, xR, yR = 0, 0, 0    
+    
+    if 0<=azim<=np.pi/2:
+        theta = (90*np.pi/180) - azim  # rotation angle
+#        print('theta angle;', theta)
+        xR = R*np.sin(theta)  # x-position of the center
+        yR = R*np.cos(theta)  # y-position of the center
+#        print('xR:',xR)
+#        print('________')
+#        print('yR:',yR)
+    if np.pi/2<azim<=np.pi:
+        theta = (360*np.pi/180) - azim + np.pi/2  # rotation angle
+#        print('theta angle;', theta)
+        xR = R*np.sin(theta)  # x-position of the center
+        yR = R*np.cos(theta)  # y-position of the center
+#        print('xR:',xR)
+#        print('________')
+#        print('yR:',yR)
+    if np.pi<azim<=3*np.pi/2:
+        theta = 2*np.pi - azim + np.pi/2  # rotation angle
+#        print('theta angle;', theta)
+        xR = R*np.sin(theta)  # x-position of the center
+        yR = R*np.cos(theta)  # y-position of the center
+#        print('xR:',xR)
+#        print('________')
+#        print('yR:',yR)
+    if 3*np.pi/2<azim<=2*np.pi: 
+        theta = 2*np.pi - azim + np.pi/2  # rotation angle
+#        print('theta angle;', theta)
+        xR = R*np.sin(theta)  # x-position of the center
+        yR = R*np.cos(theta)  # y-position of the center
+#        print('xR:',xR)
+#        print('________')
+#        print('yR:',yR)
 
     t = np.linspace(0, 2*np.pi, 100)
 
     x = xR + a*np.cos(theta)*np.cos(t) - b*np.sin(theta)*np.sin(t)
     y = yR + a*np.sin(theta)*np.cos(t) + b*np.cos(theta)*np.sin(t)
-    # plt.axis('equal')
-    plt.plot(x, y)  # rotated ellipse
+  #  plt.axis('equal')
+    plt.plot(x, y, color=color)  # rotated ellipse
 
 
-def Center(a, b, R):
+def Center(a, b, R, color):
     azim = np.linspace(0, 2*np.pi, 50)    
     for angle in azim:
-        xR = R*np.sin(angle)  # x-position of the center
-        yR = R*np.cos(angle)  # y-position of the 
-        plt.scatter(xR,yR)
-    plt.show()
+        if 0<=angle<=np.pi/2:
+            theta = (90*np.pi/180) - azim  # rotation angle
+    #        print('theta angle;', theta)
+            xR = R*np.sin(theta)  # x-position of the center
+            yR = R*np.cos(theta)  # y-position of the center
+    #        print('xR:',xR)
+    #        print('________')
+    #        print('yR:',yR)
+        if np.pi/2<angle<=np.pi:
+            theta = (360*np.pi/180) - azim + np.pi/2  # rotation angle
+    #        print('theta angle;', theta)
+            xR = R*np.sin(theta)  # x-position of the center
+            yR = R*np.cos(theta)  # y-position of the center
+    #        print('xR:',xR)
+    #        print('________')
+    #        print('yR:',yR)
+        if np.pi<angle<=3*np.pi/2:
+            theta = 2*np.pi - azim + np.pi/2  # rotation angle
+    #        print('theta angle;', theta)
+            xR = R*np.sin(theta)  # x-position of the center
+            yR = R*np.cos(theta)  # y-position of the center
+    #        print('xR:',xR)
+    #        print('________')
+    #        print('yR:',yR)
+        if 3*np.pi/2<angle<=2*np.pi: 
+            theta = 2*np.pi - azim + np.pi/2  # rotation angle
+    #        print('theta angle;', theta)
+            xR = R*np.sin(theta)  # x-position of the center
+            yR = R*np.cos(theta)  # y-position of the center
+    #        print('xR:',xR)
+    #        print('________')
+    #        print('yR:',yR)
+            plt.scatter(xR,yR, color=color)
--- gnssr4river/Fresnel/IterFresnel.py
+++ gnssr4river/Fresnel/IterFresnel.py
@@ -14,27 +14,26 @@ from FresnelZone import FirstFresnelZone, plotEllipse
 from GetOrbits import *
 
 # Usefull constants
-c = 299792458  # m.s-1 Speed of light
-L1_freq = 1575.42e6  # Hz L1 frequency
-L2_freq = 1227.60e6  # Hz L2 frequency
-lambda_L1 = c/L1_freq  # m wavelenght for L1
-lambda_L2 = c/L2_freq  # m wavelenght for L2
+c = 299792458             # m.s-1 Speed of light
+L1_freq = 1575.42e6       # Hz L1 frequency
+L2_freq = 1227.60e6       # Hz L2 frequency
+lambda_L1 = c/L1_freq     # m wavelenght for L1
+lambda_L2 = c/L2_freq     # m wavelenght for L2
 
 elev = [5,10,15]
-azim = np.linspace(0, 360, 1000)
+azim = np.linspace(0, 360, 200)
 a5,b5,R5 = FirstFresnelZone(L1_freq, 2, 5)
 a10,b10,R10 = FirstFresnelZone(L1_freq, 2, 10)
 a15,b15,R15 = FirstFresnelZone(L1_freq, 2, 15)
 
 
 for angle in azim:
-    plotEllipse(a5,b5,R5,angle)
-    plotEllipse(a10,b10,R10,angle)
-    plotEllipse(a15,b15,R15,angle)
+    plotEllipse(a5,b5,R5,angle, 'yellow')
+    plotEllipse(a10,b10,R10,angle, 'blue')
+    plotEllipse(a15,b15,R15,angle, 'red')
 
-    
 plt.xlabel("E (m)")
 plt.ylabel("N (m)")
 plt.title("Fresnel Surface")
-plt.legend()
+plt.legend(['e=5°', 'e=10°','e=15°'])
 plt.show()        
