c5cc89a17ed739ea83a0d2e89a65dfe801241699
lubin_roineau <lubin.roineau@laposte.net>
2022-07-12 10:53:59 +0200

    add Glonass frequency

--- gnssr4river/Fresnel/FresnelZone.py
+++ gnssr4river/Fresnel/FresnelZone.py
@@ -10,12 +10,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math as m
 from Geod import CarttoGeo, GeotoCart
+
 # Usefull constants
 c = 299792458               # m.s-1 Speed of light
-L1_freq = 1575.42e6         # Hz L1 frequency
-L2_freq = 1227.60e6         # Hz L2 frequency
-lambda_L1 = (c/L1_freq)     # m wavelenght for L1
-lambda_L2 = (c/L2_freq)     # m wavelenght for L2
+L1_GPS = 1575.42e6         # Hz L1 frequency for GPS
+L2_GPS = 1227.60e6         # Hz L2 frequency for GPS
+L1_Glo = 1575.42e6         # Hz L1 frequency for GPS
+L2_Glo = 1227.60e6         # Hz L2 frequency for GPS
+lambda_L1_GPS = (c/L1_GPS)     # m wavelenght for L1 GPS
+lambda_L2_GPS = (c/L2_GPS)     # m wavelenght for L2 GPS
+lambda_L1_Glo = (c/L1_Glo)     # m wavelenght for L1 Glonass
+lambda_L2_Glo = (c/L2_Glo)     # m wavelenght for L2 Glonass
+
+
 
 ###############################################################################
 
@@ -46,28 +53,43 @@ def FirstFresnelZone(freq, h, elev):
         and R meters away from the base of the Antenna.
     """
 
-    lfreq = [L1_freq, L2_freq]
+    lfreqgps = [L1_GPS, L2_GPS]
+    lfreqglo = [L1_Glo, L2_Glo]
 
-    if freq not in lfreq:
+    if freq not in lfreqgps or freq not in lfreqglo:
         raise Exception("Wrong value for L_band frequency")  
 
-    if elev>50:
-        raise Exception("Wrong value for elevation, can't excede 50° !")  
+    if elev>90:
+        raise Exception("Wrong value for elevation, can't excede 90° !")  
 
+    if freq in lfreqgps:
+        # delta = locus of points corresponding to a fixed delay;
+        # typically the first Fresnel zone is is the
+        # "zone for which the differential phase change across
+        # the surface is constrained to lambda/2" (i.e. 1/2 the wavelength)
+        d = lambda_L1_GPS/2
 
-    # delta = locus of points corresponding to a fixed delay;
-    # typically the first Fresnel zone is is the
-    # "zone for which the differential phase change across
-    # the surface is constrained to lambda/2" (i.e. 1/2 the wavelength)
-    d = lambda_L1/2
+        # from the appendix of Larson and Nievinski, 2013
+        # semi-minor axis
+        b = ((lambda_L1_GPS*h)/np.sin(m.radians(elev))) + (lambda_L1_GPS/(2*np.sin(m.radians(elev))))**2
+        b = np.sqrt(b)
+        # semi-majpr axis
+        a = b/np.sin(m.radians(elev))
 
-    # from the appendix of Larson and Nievinski, 2013
-    # semi-minor axis
-    b = ((lambda_L1*h)/np.sin(m.radians(elev))) + (lambda_L1/(2*np.sin(m.radians(elev))))**2
-    b = np.sqrt(b)
-    # semi-majpr axis
-    a = b/np.sin(m.radians(elev))
+    elif freq in lfreqglo:
+        # delta = locus of points corresponding to a fixed delay;
+        # typically the first Fresnel zone is is the
+        # "zone for which the differential phase change across
+        # the surface is constrained to lambda/2" (i.e. 1/2 the wavelength)
+        d = lambda_L1_GPS/2
 
+        # from the appendix of Larson and Nievinski, 2013
+        # semi-minor axis
+        b = ((lambda_L1_GPS*h)/np.sin(m.radians(elev))) + (lambda_L1_GPS/(2*np.sin(m.radians(elev))))**2
+        b = np.sqrt(b)
+        # semi-majpr axis
+        a = b/np.sin(m.radians(elev))
+        
     # determine distance to ellipse center in meters
     R = (h + d/np.sin(m.radians(elev))) / np.tan(m.radians(elev))
 
@@ -75,7 +97,7 @@ def FirstFresnelZone(freq, h, elev):
 
 ###############################################################################
 
-def plotEllipse(a, b, R, azim, color):
+def plotEllipse(a, b, R, lon, lat, h, azim, color):
     """
     Plot an ellipse
     
@@ -90,6 +112,8 @@ def plotEllipse(a, b, R, azim, color):
         and R meters away from the base of the Antenna.
     lon,lat: float
         position of the receiver in geographical coordinates (degrees)
+    h: float
+        hight of the receiver in meters
     azim: float
         given azimut of ellipse in degrees
     color: String
@@ -101,14 +125,15 @@ def plotEllipse(a, b, R, azim, color):
     """
     if azim > 360 or azim < 0:
         raise Exception("Wrong value azim, should be between 0 and 360!")  
+        
+    # Set coordinates of the receiver to cartesians
+    X,Y,Z = GeotoCart(lon,lat,h)
 
     azim = azim *np.pi/180
     #xR = R*np.sin(azim)  # x-position of the center
     #yR = R*np.cos(azim)  # y-position of the center
     theta = 0 
-    
-    X,Y,Z = GeotoCart(lon, lat, 0)
-    
+        
     if 0<=azim<=np.pi/2:
         theta = (90*np.pi/180) - azim  # rotation angle
 #        print('theta angle;', theta)
@@ -144,11 +169,18 @@ def plotEllipse(a, b, R, azim, color):
 
     t = np.linspace(0, 2*np.pi, 100)
 
-    x = X + a*np.cos(azim)*np.cos(t) - b*np.sin(azim)*np.sin(t)
-    y = Y + a*np.sin(azim)*np.cos(t) + b*np.cos(azim)*np.sin(t)
+    x = xR + a*np.cos(azim)*np.cos(t) - b*np.sin(azim)*np.sin(t)
+    y = yR + a*np.sin(azim)*np.cos(t) + b*np.cos(azim)*np.sin(t)
   #  plt.axis('equal')
+  
+    for i in range(len(x)):
+        z = 0
+        x[i],y[i], z = CarttoGeo(x[i],y[i],Z)
+        print("x:",x[i])
+        print('y:',y[i])
+        
     plt.plot(x, y, color=color)  # rotated ellipse
-   # plt.fill_between(x,y, color=color, alpha=0.4)
+    # plt.fill_between(x,y, color=color, alpha=0.4)
 
     return
 
@@ -192,3 +224,6 @@ def Center(a, b, R, color):
             plt.scatter(xR,yR, color=color)
     return
             
+# geo.overijssel.nl to look for db of rivers
+# rijkswaterstaa.nl to look for bd of rivers
+
